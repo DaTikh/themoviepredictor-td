f917a7a485aa8f904bf85a3dea8c53315baa60ca
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2019-10-14 23:43:37 +0200

    m parsers, m method insertQuery

--- app.py
+++ app.py
@@ -50,23 +50,27 @@ def findAll(table):
     disconnectDatabase(cnx)
     return results
 
-def insertQuery(context, action, args):
-    if context == "people":
-        k = "`firstname`, `lastname`"
-        v = f"'{args.firstname}', '{args.lastname}'"
-    elif context == "movies":
-        if action == "insert":
+def insertQuery(args):
+    context, action = args.context, args.action
+    if args.context == "people":
+        k, v = [*vars(args)], [*vars(args).values()]
+        k.remove('context'), k.remove('action'), v.remove(context), v.remove(action)
+        k, v = (", ".join(k)), (", ".join(v))
+        # v = f"'{args.firstname}', '{args.lastname}'"
+        print(v)
+    elif args.context == "movies":
+        if args.action == "insert":
             k = "`title`, `duration`, `original_title`, `origin_country`"
             v = f"'{args.title}', {args.duration}, '{args.original_title}', '{args.origin_country}'"
-        elif action == "import":
+        elif args.action == "import":
             k = "`title`, `duration`, `original_title`, `rating`, `release_date`"
             v = f"'{args.title}', {args.duration}, '{args.original_title}', '{args.rating}', '{args.release_date}'"
     return (f"INSERT INTO `{context}` ({k}) VALUES ({v})")
 
-def insert(context, action, args):
+def insert(args):
     cnx = connectToDatabase()
     cursor = createCursor(cnx)
-    cursor.execute(insertQuery(context, action, args))
+    cursor.execute(insertQuery(args))
     cnx.commit()
     closeCursor(cursor)
     disconnectDatabase(cnx)
@@ -75,8 +79,10 @@ def import_csv(args):
     with open(args.file, 'r', encoding='utf-8', newline='\n') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            row = argparse.Namespace(**row)
-            insert("movies", "import", row)
+            row = dict(row)
+            row.update({'context': args.context, 'action': args.action})
+            row = (argparse.Namespace(**row))
+            insert(row)
 
 def printPerson(person):
     print("#{}: {} {}".format(person['id'], person['firstname'], person['lastname']))
@@ -85,9 +91,12 @@ def printMovie(movie):
     print("#{}: {} released on {}".format(movie['id'], movie['title'], movie['release_date']))
 
 
-parser = argparse.ArgumentParser(description='Process MoviePredictor data')
+buffer = argparse.ArgumentParser(description="Context parser", add_help=False)
+buffer.add_argument('context', choices=['people', 'movies'], help='Le contexte dans lequel nous allons travailler')
 
-parser.add_argument('context', choices=['people', 'movies'], help='Le contexte dans lequel nous allons travailler')
+b_args, u_args = buffer.parse_known_args()
+
+parser = argparse.ArgumentParser(parents=[buffer], description='Process MoviePredictor data')
 
 action_subparser = parser.add_subparsers(title='action', dest='action')
 
@@ -97,16 +106,19 @@ list_parser.add_argument('--export' , help='Chemin du fichier exporté')
 find_parser = action_subparser.add_parser('find', help='Trouve une entité selon un paramètre')
 find_parser.add_argument('id' , help='Identifant à rechercher')
 
-insert_parser = action_subparser.add_parser('insert', help='Insère une entrée dans la base de données')
-insert_parser.add_argument('--firstname', help='Prénom')
-insert_parser.add_argument('--lastname', help='Nom de famille')
-insert_parser.add_argument('--title', help='Titre du film')
-insert_parser.add_argument('--duration', help='Durée du film')
-insert_parser.add_argument('--original-title', help='Titre original')
-insert_parser.add_argument('--origin-country', help='Pays d\'origine')
-
 import_parser = action_subparser.add_parser('import', help='Importe un fichier CSV dans la base de données')
-import_parser.add_argument('--file', help='Chemin du fichier à importer')
+import_parser.add_argument('--file', help='Chemin du fichier à importer', required=True)
+
+insert_parser = action_subparser.add_parser('insert', help='Insère une entrée dans la base de données')
+
+if b_args.context == "people":
+    insert_parser.add_argument('--firstname', help='Prénom')
+    insert_parser.add_argument('--lastname', help='Nom de famille')
+elif b_args.context == "movies":
+    insert_parser.add_argument('--title', help='Titre du film', required=True)
+    insert_parser.add_argument('--duration', help='Durée du film', required=True)
+    insert_parser.add_argument('--original-title', help='Titre original', required=True)
+    insert_parser.add_argument('--origin-country', help='Pays d\'origine', required=True)
 
 args = parser.parse_args()
 
@@ -128,7 +140,7 @@ if args.context == "people":
         for person in people:
             printPerson(person)
     if args.action == "insert":
-        insert("people", "insert", args) 
+        insert(args) 
 
 
 if args.context == "movies":
