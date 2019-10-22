66e3e5d0fd907e3f40c300264fdef448fd2e554c
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2019-10-22 16:17:06 +0200

    + list infos

--- scrapper.py
+++ scrapper.py
@@ -1,12 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("https://fr.wikipedia.org/wiki/Matrix_(film)")
+page = requests.get("https://fr.wikipedia.org/wiki/Joker_(film,_2019)")
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
 title_div = soup.find(id="firstHeading")
 title = list(title_div.children)[0].get_text()
+print(title)
 
-original_title_div = soup.find(class_="infobox_v3")
-list(original_title_div)[8]
+h2 = soup.find(id="Fiche_technique").parent
+
+fiche_ul = h2.find_next_sibling("ul")
+
+infos = fiche_ul.find_all("li")
